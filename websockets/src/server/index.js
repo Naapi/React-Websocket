6231148c0d9887e0830c48c93f38277cb888979e
Terho <terho.craven@metropolia.fi>
2019-07-03 15:33:29 +0300

    min working server

--- websockets/src/server/index.js
+++ websockets/src/server/index.js
@@ -8,7 +8,16 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+const getVisitors = () => {
+    let clients = io.sockets.clients().connected;
+    let sockets = Object.values(clients);
+    let users = sockets.map(s => s.user);
+    return users;
+};
 
+const emitVisitiors = () => {
+    io.emit("visitors", getVisitors());
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -16,9 +25,11 @@ io.on('connection', function(socket){
   socket.on("new_visitor", user => {
     console.log("new_visitor", user);
     socket.user = user;
+    emitVisitiors();
   });
 
   socket.on("disconnect", function() {
+    emitVisitiors();
     console.log("user disconnected");
   });
 
